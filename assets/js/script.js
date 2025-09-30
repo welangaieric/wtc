0320798289389b18b41b40c899c9842751a4be28
welangai <welangaieric@gmail.com>
2025-09-30 15:23:34 +0300

    minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -741,3 +741,20 @@ function setupScrollToTop() {
     });
   });
 }
+// values animation
+
+  document.querySelectorAll('.value-item').forEach(card => {
+    card.addEventListener('mousemove', e => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      card.style.setProperty('--x', `${x}px`);
+      card.style.setProperty('--y', `${y}px`);
+    });
+
+    card.addEventListener('mouseleave', () => {
+      card.style.setProperty('--x', `50%`);
+      card.style.setProperty('--y', `50%`);
+    });
+  });
+
